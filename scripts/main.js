a0291450766f7157932f7f27a3b3f0c9125518f2
Austin Carey <austinpatrickcarey@gmail.com>
2017-07-14 08:01:39 -0400

    progress

--- scripts/main.js
+++ scripts/main.js
@@ -8,32 +8,44 @@
    you'll need to parse through that first before you can start to
    write your logic.
 */
+//
+// let J = 10;
+// let Q = 10;
+// let K = 10;
+// let A = (11 || 1);
+// let card = [2, 3, 4, 5, 6, 7, 8, 9, 10, J, Q, K, A ]
+// let p1(hand);
+// let p2(hand) =
+//
+// function handValue(hand) {
+//   let phand = [];
+  // for (let i = 0; i < cards.length; i++) {
+//     let card1 = cards[i];
+//     for (let j = 0; j < cards.length; j++) {
+//       let card2 = cards[j];
+//       let uhand = [card1, card2];
+//       phand.push(uhand);
+//     }
+//   }
+//
+//   return phand === value;
+//   console.assert(phand === value);
+// }
 
-let J = 10;
-let Q = 10;
-let K = 10;
-let A = (11 || 1);
-let cards = [2, 3, 4, 5, 6, 7, 8, 9, 10, J, Q, K, A ]
-let p1(hand);
-
-let p2(hand) = 
 function handValue(hand) {
-  let phand = [];
-  for (let i = 0; i < cards.length; i++) {
-    let card1 = cards[i];
-    for (let j = 0; j < cards.length; j++) {
-      let card2 = cards[j];
-      let uhand = [card1, card2];
-      phand.push(uhand);
+  let total = 0
+  for (let i = 0; i < hand.length; i++) {
+    let card = hand[i]
+    if(card === "K" || card === "Q" || card === "J"){
+      card = 10
     }
-  }
-
-  return phand === value;
-  console.assert(phand === value);
+    if(card === "A"){
+      card = 11 || 1
+    }
+    total += Number(card)
+    }
+  return total;
 }
-
-
-
 
 
 /* -----  Hints ------
